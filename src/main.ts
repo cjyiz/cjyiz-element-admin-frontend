f7b27abb03aa1b6108daf4ae7b1d97efc2913f00
cjyiz <cjyiz@aliyun.com>
2025-03-19 09:33:23 +0800

    新增路由，并且配置基础路由

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import './style.css';
 import App from './App.vue';
 import 'virtual:svg-icons-register';
 import 'uno.css';
-import { createPinia } from "pinia";
-
-createApp(App).use(createPinia()).mount("#app");
+import { createPinia } from 'pinia';
+import router from '@/router';
+const app = createApp(App);
+const pina = createPinia();
+app.use(pina);
+app.use(router);
+app.mount('#app');
---
+++ src/views/redirect/index.vue
@@ -0,0 +1,1 @@
+<template><div>重定向</div></template>
